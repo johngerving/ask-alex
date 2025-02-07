ced247a646c1b4236324cb5f9b21728fa88e55ec
John Gerving <gervingjohn@gmail.com>
2025-02-07 22:25:29 +0000

    Fix workflow to use both document conversion and document indexing

--- backend/parse/workflow.py
+++ backend/parse/workflow.py
@@ -15,12 +15,17 @@ ray.init()
 # Get runtime environment to pass to workflow tasks
 runtime_env = json.loads(ray.get_runtime_context().get_runtime_env_string())
 
-####### Indexing Workflow #######
+####### Document Conversion Workflow #######
 
 # Define first step of workflow - getting the links
-# links = get_links.options(runtime_env=runtime_env).bind()
+links = get_links.options(runtime_env=runtime_env).bind()
 # Pass the links to convert them to documents
-# documents = convert_documents.options(runtime_env=runtime_env).bind(links)
+documents = convert_documents.options(runtime_env=runtime_env).bind(links)
+
+workflow.run(documents)
+
+####### Indexing Workflow #######
+
 # Pass the links to the indexing step of the workflow
 documents = index_documents.options(runtime_env=runtime_env).bind()
 
