d23462069ea92fc02e692da17a3605b670b2de99
John Gerving <gervingjohn@gmail.com>
2025-03-25 18:20:52 +0000

    Frontend styling

--- frontend/src/lib/components/ui/chatbubble/chatbubble.svelte
+++ frontend/src/lib/components/ui/chatbubble/chatbubble.svelte
@@ -13,7 +13,7 @@
 
 <div
 	class={cn(
-		'flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm',
+		'flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-3',
 		isUser ? 'bg-muted ml-auto' : 'bg-muted'
 	)}
 >
