c79b971cc38b6fadd8688504e134d8fba2d8e1eb
John Gerving <gervingjohn@gmail.com>
2025-04-08 17:44:52 +0000

    Support markdown responses in UI

--- frontend/src/lib/components/ui/chatbubble/chatbubble.svelte
+++ frontend/src/lib/components/ui/chatbubble/chatbubble.svelte
@@ -17,26 +17,5 @@
 		isUser ? 'bg-muted ml-auto' : 'bg-muted'
 	)}
 >
-	{message.content}
+	{@html marked(message.content)}
 </div>
-
-<!-- <div>
-	<div class={`chat-bubble flex w-full ${isUser ? 'justify-end' : 'justify-start'}`}>
-		<div
-			class={`leading-1.5 flex w-full max-w-xl flex-col rounded-b-xl border-gray-200 bg-gray-100 p-4 ${isUser ? 'rounded-l-xl' : 'rounded-r-xl'}`}
-		>
-			<div class="flex items-center space-x-2 rtl:space-x-reverse">
-				<span class="text-sm font-semibold text-gray-900">
-					{#if isUser}
-						User
-					{:else}
-						ALEX
-					{/if}
-				</span>
-			</div>
-			<div class="py-2.5 text-sm font-normal text-gray-900">
-				{@html marked(message.content)}
-			</div>
-		</div>
-	</div>
-</div> -->
