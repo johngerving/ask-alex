1f66ff4f8180ac8a7448f835b77dd93c911ff7f2
John Gerving <gervingjohn@gmail.com>
2025-04-08 15:57:45 +0000

    Add query engine

--- backend/generate/chat_flow.py
+++ backend/generate/chat_flow.py
@@ -3,16 +3,9 @@ import logging
 import os
 from textwrap import dedent
 from typing import Annotated, Dict, Iterator, Optional, Literal, List
+from urllib.parse import urlparse
 import haystack
 from pydantic import BaseModel, Field
-
-from haystack.utils import Secret
-from haystack_integrations.document_stores.pgvector import PgvectorDocumentStore
-from haystack_integrations.components.retrievers.pgvector import (
-    PgvectorEmbeddingRetriever,
-)
-from haystack.components.embedders import SentenceTransformersTextEmbedder
-from haystack import Pipeline
 
 from llama_index.core.workflow import (
     Event,
@@ -22,52 +15,13 @@ from llama_index.core.workflow import (
     Workflow,
     step,
 )
-
 from llama_index.llms.google_genai import GoogleGenAI
 from llama_index.core.agent.workflow import ReActAgent, FunctionAgent
 from llama_index.core.tools import FunctionTool
 from llama_index.core.llms import ChatMessage
-
-document_store = PgvectorDocumentStore(
-    connection_string=Secret.from_token(os.getenv("PG_CONN_STR")),
-    table_name="haystack_docs",
-    embedding_dimension=768,
-    vector_function="cosine_similarity",
-    recreate_table=False,
-    search_strategy="hnsw",
-)
-
-embedder = SentenceTransformersTextEmbedder()
-embedder.warm_up()
-
-# Retriever to get embedding vectors based on query
-retriever = PgvectorEmbeddingRetriever(document_store=document_store, top_k=8)
-
-pipeline = Pipeline()
-
-pipeline.add_component("embedder", embedder)  # Get query vector embedding
-pipeline.add_component("retriever", retriever)
-
-pipeline.connect("embedder.embedding", "retriever.query_embedding")
-
-
-def search_knowledge_base(query: Annotated[str, "The query to search for."]) -> str:
-    """Use this function to search the knowledge base for information about a query."""
-
-    res = pipeline.run({"embedder": {"text": query}})
-
-    documents: List[haystack.Document] = res["retriever"]["documents"]
-    documents = [
-        {"doc_id": documents[i].id[:8], "content": documents[i].content}
-        for i in range(len(documents))
-    ]
-    logger = logging.getLogger("ray.serve")
-    logger.info(documents)
-
-    return json.dumps(documents, indent=2)
-
-
-tool = FunctionTool
+from llama_index.core import VectorStoreIndex, StorageContext
+from llama_index.vector_stores.postgres import PGVectorStore
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 
 class WorkflowStartEvent(StartEvent):
@@ -102,6 +56,41 @@ class ChatFlow(Workflow):
     llm = GoogleGenAI(model="gemini-2.0-flash")
 
     small_llm = GoogleGenAI(model="gemini-2.0-flash-lite")
+
+    # Get Postgres credentials from connection string
+    pg_url = urlparse(os.getenv("PG_CONN_STR"))
+    host = pg_url.hostname
+    port = pg_url.port
+    database = pg_url.path[1:]
+    user = pg_url.username
+    password = pg_url.password
+
+    # Vector store to store chunks + embeddings in
+
+    vector_store = PGVectorStore.from_params(
+        host=host,
+        port=port,
+        database=database,
+        user=user,
+        password=password,
+        table_name="llamaindex_docs",
+        schema_name="public",
+        embed_dim=768,
+        hnsw_kwargs={
+            "hnsw_m": 16,
+            "hnsw_ef_construction": 64,
+            "hnsw_ef_search": 40,
+            "hnsw_dist_method": "vector_cosine_ops",
+        },
+    )
+
+    index = VectorStoreIndex.from_vector_store(
+        vector_store=vector_store,
+        embed_model=HuggingFaceEmbedding(
+            model_name="sentence-transformers/all-mpnet-base-v2"
+        ),
+    )
+    query_engine = index.as_query_engine(llm=llm)
 
     @step
     async def route_chat_or_retrieval(
@@ -166,40 +155,9 @@ class ChatFlow(Workflow):
     async def retrieve(self, ctx: Context, ev: RetrievalRouteEvent) -> StopEvent:
         """Handle the retrieval route by searching the knowledge base for information."""
 
-        message = await ctx.get("message")
-        history = await ctx.get("history")
+        message: ChatMessage = await ctx.get("message")
+        history: List[ChatMessage] = await ctx.get("history")
 
-        tool = FunctionTool.from_defaults(search_knowledge_base)
-
-        agent = ReActAgent(
-            system_prompt=dedent(
-                """\
-                You are ALEX, a helpful AI assistant designed to provide information about Humboldt. Respond to the input as a friendly AI assistant, generating human-like text, and follow the instructions in the input if applicable. Keep the response concise and engaging, using Markdown when appropriate. Use a conversational tone and provide helpful and informative responses. Do not provide information beyond what you are given in the context.
-                Formulate an answer to user queries.
-                Follow these steps:
-                1. Think step-by-step about the query, expanding with more context if necessary.
-                2. ALWAYS use the `search_knowledge_base(query)` tool to get relevant documents.
-                3. Use the retrieved documents to write a comprehensive answer to the query, discarding irrelevant documents. Provide inline citations of each document you use.
-                Finally, here are a set of rules that you MUST follow:
-                <rules>
-                - Use the `search_knowledge_base(query)` tool to retrieve documents from your knowledge base before answering the query.
-                - Do not use phrases like "based on the information provided" or "from the knowledge base".
-                - Always provide inline citations for any information you use to formulate your answer. These should be in the format [doc_id], where doc_id is the id of the document you are citing. Multiple citations should be written as [id_1][id_2].
-                </rules>
-                """
-            ),
-            llm=self.llm,
-            tools=[tool],
-        )
-
-        from llama_index.core.agent.workflow import AgentStream, ToolCallResult
-
-        handler = agent.run(message, chat_history=history)
-
-        async for ev in handler.stream_events():
-            if isinstance(ev, AgentStream):
-                print(f"{ev.delta}", end="", flush=True)
-
-        response = await handler
+        response = self.query_engine.query(message.content)
 
         return StopEvent(result=response)
